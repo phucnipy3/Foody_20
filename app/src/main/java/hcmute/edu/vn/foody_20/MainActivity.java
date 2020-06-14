201e9622655f5d408298763d567189c96792597c
Phúc Nguyễn Huỳnh <phucnipy3@gmail.com>
2020-06-14 15:07:50 +0700

    add connection

--- app/src/main/java/hcmute/edu/vn/foody_20/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/foody_20/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtProvinces;
@@ -31,6 +37,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    // Set the connection string
+                    Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+                    String username = "phucni";
+                    String password = "12345678";
+
+                    Connection DBconn = DriverManager.getConnection("jdbc:jtds:sqlserver://Nhom20FoodyDB.mssql.somee.com/Nhom20FoodyDB;user="+username+";password="+password);
+                    Log.e("Connection","open");
+                    Statement stmt = DBconn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery("select * from Province");
+                    while(resultSet.next()){
+                        String value = resultSet.getString("Name");
+                        Log.e("Connection",value);
+                    }
+                    DBconn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
 
     }
 }
